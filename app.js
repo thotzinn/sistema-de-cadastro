2f937f5dcb87b13d61cb1c2f9b9b675c1de1c730
Júlio César <thotzinn@gmail.com>
2023-07-15 11:56:54 -0300

    Commit que será descartado

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 //Executando o push
-console.log('Fazendo um push pelo git no computador')
+console.log('Fazendo um push pelo git no computador');
+//Console que será retirado
+console.log('Eu serei retornado para o estado anterior');
