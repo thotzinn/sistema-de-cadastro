ca318dfac0fc0b55ddec1551ca0b8344027eccc5
Júlio César <thotzinn@gmail.com>
2023-07-15 12:06:21 -0300

    Voltando ao estado anterior

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 //Executando o push
-console.log('Fazendo um push pelo git no computador');
-//Console que será retirado
-console.log('Eu serei retornado para o estado anterior');
+console.log('Fazendo um push pelo git no computador')
